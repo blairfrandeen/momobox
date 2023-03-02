15c8191d3382067e0d460e0c007b9b7a6b3bed36
Blair Frandeen <blairfrandeen@gmail.com>
2023-03-02 15:58:16 -0800

    MVP without hall sensor

--- src/code.py
+++ src/code.py
@@ -29,31 +29,35 @@ RESET_CLOCK_S = 5
 
 
 class AudioPlayer:
-    def __init__(self, hall_sensor):
+    def __init__(self):
         self.audio = audiopwmio.PWMAudioOut(board.GP16)
-        self.song = None
+        self.song: Option[str] = None  # RFID string for last song
         self.decoder = None
-        self.hall_sensor = hall_sensor  # GPIO Pin 18
+        #  self.hall_sensor = hall_sensor  # GPIO Pin 18
         self.reset_clock = 0
         self.rfid_reader = rfid_reader_init()
 
         self.listen()
 
-    def play(self, filename):
-        self.song = filename
-        self.decoder = audiomp3.MP3Decoder(open(self.song, "rb"))
+    def play(self):
+        self.decoder = audiomp3.MP3Decoder(open(LIBRARY[self.song], "rb"))
         self.audio.play(self.decoder)
+        print("Playing a song for you...")
+        #  while self.audio.playing:
+        #  if not check_rfid(self.rfid_reader, self.song):
+        #  print("Momie removed!")
+        #  self.pause()
+        #  break
 
     def pause(self):
         self.audio.pause()
         self.reset_clock = time.time()
+        #  while self.audio.paused:
+        #  if check_rfid(self.rfid_reader, self.song):
+        #  self.resume()
 
     def resume(self):
-        time_paused = time.time() - self.reset_clock
-        if time_paused < RESET_CLOCK_S:
-            self.audio.resume()
-        else:
-            self.audio.stop()
+        self.audio.resume()
 
     def unload(self):
         self.audio.stop()
@@ -67,25 +71,38 @@ class AudioPlayer:
         return self.audio.paused
 
     def listen(self):
+        # State machine
         while True:
-            if (
-                self.hall_sensor.value is True
-                and not self.is_playing
-                and not self.is_paused
-            ):
-                self.get_input()
-            if self.hall_sensor.value is False:
-                print("Hall sensor disconnected.")
-                if not self.is_paused:
+            if self.is_paused:
+                # Check RFID chip
+                rfid_key = read_rfid(self.rfid_reader)
+                # If same as current song, then resume
+                time_paused = time.time() - self.reset_clock
+                if rfid_key == self.song and time_paused < RESET_CLOCK_S:
+                    self.resume()
+                # If different song, start that song
+                else:
+                    self.song = rfid_key
+                    self.play()
+            if self.is_playing:
+                # Check to make sure the momie is still there
+                if not check_rfid(self.rfid_reader, self.song):
+                    print("Momie removed!")
                     self.pause()
-                time.sleep(1)
-            if self.hall_sensor.value is True and self.is_paused:
-                self.resume()
+            else:  # not paused or playing i.e. stopped
+                # We need one failed RFID check before we start playing
+                # the same song again. This prevents songs from
+                # repeating if a momie is left attached.
+                if self.song is not None and check_rfid(self.rfid_reader, self.song):
+                    continue
+                print("Place RFID card on sensor to select song")
+                self.song = read_rfid(self.rfid_reader)
+                self.play()
 
     def get_input(self):
         print("Place RFID card on sensor to select song")
-        song = read_rfid(self.rfid_reader)
-        self.play(LIBRARY[song])
+        self.song = read_rfid(self.rfid_reader)
+        self.play()
 
 
 def rfid_reader_init():
@@ -100,6 +117,23 @@ def rfid_reader_init():
 
     rfid.set_antenna_gain(0x07 << 4)
     return rfid
+
+
+def check_rfid(rfid_reader, expected_data):
+    for _ in range(4):
+        (status, tag_type) = rfid_reader.request(rfid_reader.REQALL)
+        #  print("First Check: ", status, tag_type)
+        if status == rfid_reader.OK:
+            (status, raw_uid) = rfid_reader.anticoll()
+            #  print("Second Check: ", status, raw_uid)
+            if status == rfid_reader.OK:
+                rfid_data = "{:02x}{:02x}{:02x}{:02x}".format(
+                    raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]
+                )
+                if rfid_data == expected_data:
+                    return True
+                return False
+    return False
 
 
 def read_rfid(rfid_reader):
@@ -130,9 +164,9 @@ def main():
     vfs = storage.VfsFat(sdcard)
     storage.mount(vfs, "/sd")
 
-    p18 = digitalio.DigitalInOut(board.GP18)
+    #  p18 = digitalio.DigitalInOut(board.GP18)
     #  read_rfid()
-    player = AudioPlayer(p18)
+    player = AudioPlayer()
 
 
 if __name__ == "__main__":
